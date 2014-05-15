d6bab4db9d2b21353a49dfadab7ab85c3f654260
runningtalus <dave@davemay.org>
2014-05-15 10:19:20 -0600

    Adding API key

--- controllers/weather.js
+++ controllers/weather.js
@@ -1,9 +1,16 @@
 var weatherController = {
 	index: function(req, res) {
-		res.send({
-			temperature: 65
+
+		WeatherAPI.get(function(data){
+			res.send({
+				temperature: 65
+			});
 		});
 	}
 };
+
+var request = require('request'); 
+//require statements are on a per file basis (i.e. SCOPE)
+//npm install --save request
 
 module.exports = weatherController;
